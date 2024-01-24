729adb2eee4465eb051a8c0466ad1acb30c65540
sonomousekou <sonomousekou@gmail.com>
2024-01-24 09:40:58 +0000

    update home

--- views/client/includes/header.inc.php
+++ views/client/includes/header.inc.php
@@ -14,11 +14,11 @@
                   <div class="menu-wrapper d-flex align-items-center justify-content-between">
                     <div class="header-left d-flex align-items-center justify-content-between">
                       <div class="logo logo-large light-logo">
-                        <a href="index.html"><img src="./public/assets/images/logo/RECOSAC.png" alt="logo"></a>
+                        <a href="index.php?route=home"><img src="./public/assets/images/logo/RECOSAC.png" alt="logo"></a>
                       </div>
                       <!-- Logo Mobile-->
                       <div class="logo logo-mobile light-logo">
-                        <a href="index.html"><img src="./public/assets/images/icon/favicon.png" alt="img"></a>
+                        <a href="index.php?route=home"><img src="./public/assets/images/icon/favicon.png" alt="img"></a>
                       </div>
                     </div>
                     <div class="search-container">
